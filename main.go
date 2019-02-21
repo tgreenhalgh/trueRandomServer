1443a6efa2c7b0a0e1bae4d5dba1db9643a637b8
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-02-20 16:32:25 -0800

    using a template to create an html file

--- main.go
+++ main.go
@@ -1,62 +1,25 @@
 package main
 
-import "fmt"
-
-type person struct {
-	fname string
-	lname string
-}
-
-// (p person) attaches this func to the struct `person`
-func (p person) speak() {
-	fmt.Println(p.fname, `says, "Good morning!"`)
-}
-
-type secretAgent struct {
-	person
-	licenseToKill bool
-}
-
-func (sa secretAgent) speak() {
-	fmt.Println(sa.fname, sa.lname, `says, "Shaken, not stirred."`)
-
-}
-
-type human interface {
-	speak()
-}
-
-func saySomething(h human) {
-	h.speak()
-}
+import (
+	"log"
+	"os"
+	"text/template"
+)
 
 func main() {
-	xi := []int{2, 4, 5, 4, 3}
-	fmt.Println(xi)
-
-	m := map[string]string{
-		"Name": "Thomas",
-		"Age":  "50",
-	}
-	fmt.Println(m)
-
-	p1 := person{
-		fname: "Thomas",
-		lname: "Greenhalgh",
-	}
-	fmt.Println(p1)
-	// p1.speak()
-
-	sa1 := secretAgent{
-		person{
-			"James",
-			"Bond",
-		},
-		true,
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	// sa1.speak()
-	// sa1.person.speak()
-	saySomething(p1)
-	saySomething(sa1)
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Println("error creating the file", err)
+	}
+	defer nf.Close()
+
+	err = tpl.Execute(nf, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
